a70b5f283b21df1887c6963fd9f5a04c179dbe94
Wojciech Piech <129212595+wojpo@users.noreply.github.com>
2026-01-05 12:20:24 +0100

    base for elo guessing

--- app/pages/chess.vue
+++ app/pages/chess.vue
@@ -2,6 +2,28 @@
 import type { ChessGame } from '~~/types/chesscom'
 
 const { data, pending } = await useFetch<ChessGame>('/api/random_game')
+
+const guessedElo = ref<number | null>(null)
+
+const averageElo = computed(() => {
+  if (!data.value) return null
+
+  const white = data.value.white.rating
+  const black = data.value.black.rating
+  const avg = (white + black) / 2
+
+  return Math.ceil(avg / 50) * 50
+})
+
+const submitGuess = () => {
+  if (guessedElo.value === null || averageElo.value === null) return
+
+  const diff = Math.abs(guessedElo.value - averageElo.value)
+
+  console.log(`Real avg ELO: ${averageElo.value}`)
+  console.log(`User guess: ${guessedElo.value}`)
+  console.log(`Off by: ${diff}`)
+}
 </script>
 
 <template>
@@ -9,12 +31,31 @@ const { data, pending } = await useFetch<ChessGame>('/api/random_game')
     <div v-if="pending">
       Loading...
     </div>
-    <div v-else-if="data">
+    <div
+      v-else-if="data"
+      class="flex flex-col items-center"
+    >
       <ChessBoard
         v-if="data.pgn"
         :pgn="data.pgn"
         :time-control="data.time_control"
       />
+      <form
+        class="flex gap-3"
+        @submit.prevent="submitGuess"
+      >
+        <UInputNumber
+          v-model="guessedElo"
+          placeholder="Guess average ELO"
+          :min="400"
+          :max="3500"
+          required
+        />
+
+        <UButton type="submit">
+          Submit
+        </UButton>
+      </form>
     </div>
   </div>
 </template>
